a20f7b64493a6a1524d007e83b15283fd152fc7f
Ayuuu0424 <payushi9877@gmail.com>
2026-06-17 20:54:10 +0530

    feat: Add new pages and assets for About, Feedback, Help Center, Order Now, Partner, Privacy Policy, Sitemap, and Terms of Service

--- CravingsReTail/Client/src/App.jsx
+++ CravingsReTail/Client/src/App.jsx
@@ -4,11 +4,19 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
+import About from "./pages/About";
 import Contactus from "./pages/Contactus";
+import Feedback from "./pages/Feedback";
+import HelpCenter from "./pages/HelpCenter";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import OrderNow from "./pages/OrderNow";
+import Partner from "./pages/Partner";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Register from "./pages/Register";
+import RestaurantCard from "./pages/RestaurantCard";
+import Sitemap from "./pages/Sitemap";
+import TermsOfService from "./pages/TermsOfService";
 
 const App = () => {
   return (
@@ -18,9 +26,14 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contactus" element={<Contactus />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/helpcenter" element={<Helpcenter />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/ordernow" element={<OrderNow />} />
+          <Route path="/partner" element={<Partner />} />
+          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           <Route path="/register" element={<Register />} />
         </Routes>
 
